667474b2882df2594e92608a129fbda28945105a
Tiago Biazin <131045933+Biazindev@users.noreply.github.com>
2024-02-12 14:01:05 -0300

    envio de arquivo contendo de programação

---
+++ txt.java
@@ -0,0 +1,16 @@
+Lógica Acordando...
+
+1 - Ouço celular despertando
+2 - desligo o alarme
+3 - levanto e vou ao banheiro
+4 - escovar dentes
+5 - acordo a esposa para preparar o café
+6 - tomamos café
+7- se for de segunda a sexta
+   * vou para escritorio ao lado de minha casa
+   *inicio a rotina de trabalho
+   * cumpro o horario expediente
+8- se for feriado ou fim de semana
+   * começo organizando tarefas caseiras
+   * curso EBAC
+   * folga 
